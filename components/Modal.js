0f29b53a13f94d255760900731ce56486367d8c5
Michelle Castaneda <michellesaucedas@gmail.com>
2023-08-29 14:28:12 -0500

    step 1 Planning

---
+++ components/Modal.js
@@ -0,0 +1,6 @@
+// display any content passed to it as children, and should have a close button to dismiss the modal.
+
+// Your modal should also have the following features:
+
+// The modal should be able to be dismissed by clicking outside of it.
+// The modal should be able to be dismissed by pressing the escape key.
